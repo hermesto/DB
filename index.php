199f55876d9757ddbdff96dc755d4638a3ebdce0
hermesto <ernesto.gmz.zcrs@gmail.com>
2020-01-13 21:41:25 -0600

    index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div style="width:300px;">
+    <form action="insert.php" method="POST">
+    <input type="text" value="" placeholder="nombre" id="nombre" name="nombre">
+    <button type="submit">guardar</button>
+    </form>
+    </div>
+</body>
+</html>
--- insert.php
+++ insert.php
@@ -1,9 +1,10 @@
 <?php
   require_once('settings.config.php');          // Define db configuration arrays here
   require_once('DBConnection.php');             // Include this file
- 
+  $test3 = $_POST["nombre"];
+  echo $test3;
   $database = new DBConnection($localhost);      // Create new connection by passing in your configuration array
-  $test3 = "hermest";
+//   $test3 = "hermest";s
   $sqlInsert = "insert into usurios(nombre) VALUES('".$test3."');";              // Insert/Update/Delete Statements:
   $count = $database->runQuery($sqlInsert);     // Use this method to run inserts/updates/deletes
   echo "number of records inserted: " . $count;
